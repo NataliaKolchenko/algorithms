e3a8f99388ef4844dd3520ee3a8321a1e0abb341
Natalia <kolchenko.nata.yu@gmail.com>
2024-06-16 22:00:32 +0200

    HomeWork_1: Sum

---
+++ HomeWork_1/src/main/java/werwefw/Sum.java
@@ -0,0 +1,16 @@
+package werwefw;
+
+public class Sum {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        int sum;
+
+        sum = a + b + c;
+
+        System.out.println(a + " + " + b + " + "+ c + " = " + sum);
+    }
+}
